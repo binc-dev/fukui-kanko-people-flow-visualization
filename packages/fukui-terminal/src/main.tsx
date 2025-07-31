a1a650c76f6422f614dfb78f5d1b6b58d240e71e
kuwabara <kuwabara@jig.jp>
2025-07-31 16:06:56 +0900

    👍 ヘッダーを追加した

--- packages/fukui-terminal/src/main.tsx
+++ packages/fukui-terminal/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { Header } from "./components/parts/header";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Header />
     <App />
   </StrictMode>,
 );
