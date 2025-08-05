e2df7a01a3ec38d31a5f41ad001f1cd14661d433
kuwabara <kuwabara@jig.jp>
2025-08-05 15:21:48 +0900

    👍 csvダウンロードボタンの見た目変更とレイアウト調整

--- packages/fukui-terminal/src/components/parts/period-graph-panel.tsx
+++ packages/fukui-terminal/src/components/parts/period-graph-panel.tsx
@@ -6,6 +6,7 @@ import { StatsSummary } from "@/components/parts/stats-summary";
 import { Card, CardContent } from "@/components/ui/card";
 import { AggregatedData } from "@/interfaces/aggregated-data.interface";
 import { Period } from "@/interfaces/period.interface";
+import { DownloadIcon } from "@primer/octicons-react";
 import { saveAs } from "file-saver";
 
 type PeriodGraphPanelProps = {
@@ -34,63 +35,66 @@ export function PeriodGraphPanel({
   filteredData,
   filteredDailyData,
 }: PeriodGraphPanelProps) {
-  const handleDownloadCSV = () => {
-    const csv = convertToCSV(filteredData);
+  const handleDownloadCSV = (data: AggregatedData[]) => {
+    const csv = convertToCSV(data);
     const blob = new Blob([csv], { type: "text/csv;charset=utf-8" });
-    saveAs(blob, "filteredData.csv");
-  };
-  const handleDownloadDailyCSV = () => {
-    const csv = convertToCSV(filteredDailyData);
-    const blob = new Blob([csv], { type: "text/csv;charset=utf-8" });
-    saveAs(blob, "filteredDailyData.csv");
+    saveAs(blob, "fukui-terminal-data.csv");
   };
 
   return (
     <div className="w-full flex flex-col items-center">
-      {theme === "month" && (
-        <MonthRangePicker
-          startMonth={period.startMonth}
-          endMonth={period.endMonth}
-          onChange={(start, end) => {
-            setPeriod((prev) => ({ ...prev, startMonth: start, endMonth: end }));
-          }}
-        />
-      )}
-
-      {theme === "week" && (
-        <RangeSelector
-          type="week"
-          start={period.startWeekRange}
-          end={period.endWeekRange}
-          setStart={(range) => setPeriod((prev) => ({ ...prev, startWeekRange: range }))}
-          setEnd={(range) => setPeriod((prev) => ({ ...prev, endWeekRange: range }))}
-        />
-      )}
-
-      {(theme === "day" || theme === "hour") && (
-        <RangeSelector
-          type="date"
-          start={period.startDate}
-          end={period.endDate}
-          setStart={(date) => setPeriod((prev) => ({ ...prev, startDate: date }))}
-          setEnd={(date) => setPeriod((prev) => ({ ...prev, endDate: date }))}
-        />
-      )}
-
-      <button
-        className="mb-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-        onClick={handleDownloadCSV}
-        disabled={filteredData.length === 0}
+      <div
+        className={`flex gap-2 w-full items-end justify-center ${
+          !isCompareMode ? "sm:pl-[88px]" : ""
+        }`}
       >
-        CSVダウンロード
-      </button>
-      <button
-        className="mb-2 px-4 py-2 bg-red-500 text-white rounded hover:bg-blue-600"
-        onClick={handleDownloadDailyCSV}
-        disabled={filteredDailyData.length === 0}
-      >
-        CSVダウンロード
-      </button>
+        {theme === "month" && (
+          <MonthRangePicker
+            startMonth={period.startMonth}
+            endMonth={period.endMonth}
+            onChange={(start, end) => {
+              setPeriod((prev) => ({ ...prev, startMonth: start, endMonth: end }));
+            }}
+          />
+        )}
+
+        {theme === "week" && (
+          <RangeSelector
+            type="week"
+            start={period.startWeekRange}
+            end={period.endWeekRange}
+            setStart={(range) => setPeriod((prev) => ({ ...prev, startWeekRange: range }))}
+            setEnd={(range) => setPeriod((prev) => ({ ...prev, endWeekRange: range }))}
+          />
+        )}
+
+        {(theme === "day" || theme === "hour") && (
+          <RangeSelector
+            type="date"
+            start={period.startDate}
+            end={period.endDate}
+            setStart={(date) => setPeriod((prev) => ({ ...prev, startDate: date }))}
+            setEnd={(date) => setPeriod((prev) => ({ ...prev, endDate: date }))}
+          />
+        )}
+
+        <button
+          className="h-9 px-3 bg-blue-500 text-white rounded hover:bg-blue-600 disabled:bg-gray-300 disabled:text-gray-500 disabled:cursor-not-allowed"
+          onClick={() => handleDownloadCSV(theme === "hour" ? filteredDailyData : filteredData)}
+          disabled={
+            !(
+              (period.startMonth && period.endMonth) ||
+              (period.startWeekRange && period.endWeekRange) ||
+              (period.startDate && period.endDate)
+            )
+          }
+        >
+          <span className="flex items-center gap-1">
+            <DownloadIcon size={16} />
+            {!isCompareMode && <span className="hidden sm:inline">CSV</span>}
+          </span>
+        </button>
+      </div>
 
       <div className="w-full flex flex-col items-center justify-end min-h-[40vh]">
         <Card
